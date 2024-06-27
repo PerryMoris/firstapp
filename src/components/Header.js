0064f6923947e7c2d2963104fb8aba5d9e3c1626
PerryMoris <morison.nasia@gmail.com>
2024-06-27 15:39:22 +0000

    buttom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import logo from '../user.png';
+import {Link} from "react-router-dom"
 
 function Header(){
     return (
         <nav className='App-header'>
             <h1>ISD-Team</h1>
             <div className='nav--right'>
-            <h4>Project Tracker</h4>
-            <div className='profile'>
-                <a href='#'><img src={logo} alt='logo'/></a>
-            </div>
-            
-                
+                <h4>Project Tracker</h4>
+                <Link to="/" ><a className='nav--button'>Home</a></Link>
+                <Link to="/projects" ><a className='nav--button'>Projects</a></Link>
+                <div className='profile'>
+                    <Link to='/'><img src={logo} alt='logo'/></Link>
+                </div>   
             </div>
             
         </nav>
