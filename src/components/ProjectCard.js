2c5933b7b4ae1d28a29f04273c829c64c3515ec8
PerryMoris <morison.nasia@gmail.com>
2024-07-01 09:38:07 +0000

    b

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import {Link} from "react-router-dom"
 
 export default function ProjectCard(props){
     return (
@@ -8,6 +8,7 @@ export default function ProjectCard(props){
             <h1>{props.name}</h1>
             <h3>{props.details}</h3>
             <p>Started: {props.date}</p>
+            <Link to="/projects" style={{ textDecoration: 'none'}}><p className='card--button'>Open</p></Link>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,30 +5,35 @@ import {Link} from "react-router-dom"
 export default function Projects(){
   const pjects =[
     {
+      id: 1,
       name: "Gallop 1",
       details: "Inspection of all galop schools in the country",
       date: "27/12/2024",
       closed: false,
     },
     {
+      id: 2,
       name: "Gallop 2",
       details: "Inspection of all galop schools in the country, from basic to SHS, all pretertiary schools",
       date: "04/06/2024",
       closed: true,
     },
     {
+      id: 3,
       name: "BigWin",
       details: "Inspection of all selected schools in the country, for bigwin project",
       date: "04/09/2023",
       closed: true,
     },
     {
+      id: 4,
       name: "CEPS",
       details: "Inspection of all selected schools in the country, for bigwin project",
       date: "04/06/2024",
       closed: false,
     },
     {
+      id: 5,
       name: "Category of concern",
       details: "Inspection of all selected schools in the country, for bigwin project",
       date: "04/06/2024",
@@ -37,10 +42,8 @@ export default function Projects(){
   ]
   const allp = pjects.map((projects) => {
     return <ProjectCard 
-                name={projects.name}
-                details={projects.details}
-                date={projects.date}
-                closed={projects.closed}
+              key={projects.id}
+                {...projects}
               />
   })
     return (
