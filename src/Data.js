f7edfa8e81bc4d4319d00a340d678a600bdc1bd7
PerryMoris <morison.nasia@gmail.com>
2024-07-01 10:24:20 +0000

    if else

---
+++ src/Data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: "Gallop 1",
+      details: "Inspection of all galop schools in the country",
+      date: "27/12/2024",
+      closed: false,
+    },
+    {
+      id: 2,
+      name: "Gallop 2",
+      details: "Inspection of all galop schools in the country, from basic to SHS, all pretertiary schools",
+      date: "04/06/2024",
+      closed: true,
+    },
+    {
+      id: 3,
+      name: "BigWin",
+      details: "Inspection of all selected schools in the country, for bigwin project",
+      date: "04/09/2023",
+      closed: true,
+    },
+    {
+      id: 4,
+      name: "CEPS",
+      details: "Inspection of all selected schools in the country, for bigwin project",
+      date: "04/06/2024",
+      closed: false,
+    },
+    {
+      id: 5,
+      name: "Category of concern",
+      details: "Inspection of all selected schools in the country, for bigwin project",
+      date: "04/06/2024",
+      closed: true,
+    },
+  ]
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,8 +5,8 @@ export default function ProjectCard(props){
     return (
         <div className="projectCard">
             {props.closed && <div className="card--badge">Closed</div>}
-            <h1>{props.name}</h1>
-            <h3>{props.details}</h3>
+            <h1>{props.name.length > 10 ? props.name.substring(0, 10) + "..." : props.name}</h1>
+            <h3>{props.details.length > 35 ? props.details.substring(0, 32) + "..." : props.details}</h3>
             <p>Started: {props.date}</p>
             <Link to="/projects" style={{ textDecoration: 'none'}}><p className='card--button'>Open</p></Link>
         </div>
