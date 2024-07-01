3cd7e3135972567827ed28b1a5f89840cba54a87
PerryMoris <morison.nasia@gmail.com>
2024-07-01 10:29:05 +0000

    horizontal line

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,6 +6,7 @@ export default function ProjectCard(props){
         <div className="projectCard">
             {props.closed && <div className="card--badge">Closed</div>}
             <h1>{props.name.length > 10 ? props.name.substring(0, 10) + "..." : props.name}</h1>
+            <hr/>
             <h3>{props.details.length > 35 ? props.details.substring(0, 32) + "..." : props.details}</h3>
             <p>Started: {props.date}</p>
             <Link to="/projects" style={{ textDecoration: 'none'}}><p className='card--button'>Open</p></Link>
