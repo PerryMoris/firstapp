805a29d2aa06d944c888884244043d2b224b732b
PerryMoris <morison.nasia@gmail.com>
2024-07-02 16:08:17 +0000

    ju

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from '../user.png';
-import {Link} from "react-router-dom"
+import {Link, Navigate} from "react-router-dom"
+
+
 
 function Header(){
     return (
@@ -10,6 +12,7 @@ function Header(){
                 <h4>Project Tracker</h4>
                 <Link to="/" style={{ textDecoration: 'none'}}><p className='nav--button'>Home</p></Link>
                 <Link to="/projects" style={{ textDecoration: 'none'}} ><p className='nav--button'>Projects</p></Link>
+               
                 <div className='profile'>
                     <Link to='/profile'><img src={logo} alt='logo'/></Link>
                 </div>   
