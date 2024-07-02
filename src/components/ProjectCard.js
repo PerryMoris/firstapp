be5424f822bc4e0c698df7439ee09477eb51490a
PerryMoris <morison.nasia@gmail.com>
2024-07-02 10:08:02 +0000

    api integration

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,11 +4,11 @@ import {Link} from "react-router-dom"
 export default function ProjectCard(props){
     return (
         <div className="projectCard">
-            {props.closed && <div className="card--badge">Closed</div>}
+            {props.status == "Closed" ? <div className="card--badge">{props.status}</div> : <div className="card--badge-b">{props.status}</div>}
             <h1>{props.name.length > 10 ? props.name.substring(0, 10) + "..." : props.name}</h1>
             <hr/>
             <h3>{props.details.length > 35 ? props.details.substring(0, 32) + "..." : props.details}</h3>
-            <p>Started: {props.date}</p>
+            <p>Started: {props.startdate}</p>
             <Link to="/projects" style={{ textDecoration: 'none'}}><p className='card--button'>View Details</p></Link>
         </div>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,9 +2,31 @@ import React from 'react'
 import ProjectCard from "./ProjectCard"
 // import {Link} from "react-router-dom"
 import pjects from "../Data"
-export default function Projects(){
+import {useEffect, useState} from 'react'
 
-  const allp = pjects.map((projects) => {
+
+export default function Projects(){
+  const [data, setData] = useState([])
+  const API_URL = 'http://127.0.0.1:8000/api/projects/'
+  useEffect(() => {
+    async function fetchData(){
+      try {
+        const response = await fetch(`${API_URL}`)
+        if (!response.ok){
+          throw new Error('Network response was not successfull');
+        }
+        const result = await response.json()
+        console.log(result)
+        setData(result)
+      } catch (error) {
+        console.error('Error fetching data: ', error)
+      }
+    }
+
+    fetchData();
+  }, []);
+  console.log(data)
+  const allp = data.map((projects) => {
     return <ProjectCard 
               key={projects.id}
                 {...projects}
