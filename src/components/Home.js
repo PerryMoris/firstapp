38efb6d591eb0705087c97252714d02f02713978
PerryMoris <morison.nasia@gmail.com>
2024-07-02 15:04:48 +0000

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Mytasks from './Mytasks'
 
 export default function Home(){
     return (
         <div >
             <h1>Home page</h1>
+            <Mytasks />
         </div>
     )
 }
