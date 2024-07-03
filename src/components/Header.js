42e227bc49ecb701440c5eb87e29f62883628ee3
PerryMoris <morison.nasia@gmail.com>
2024-07-03 12:13:57 +0000

    h

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,7 @@ function Header(){
                 <Link to="/" style={{ textDecoration: 'none'}}><p className='nav--button'>Home</p></Link>
                 <Link to="/projects" style={{ textDecoration: 'none'}} ><p className='nav--button'>Projects</p></Link>
                 <Link to="/projects" style={{ textDecoration: 'none'}} ><p className='nav--button'>Tasks</p></Link>
+                <Link to="/register" style={{ textDecoration: 'none'}} ><p className='nav--button'>Register</p></Link>
                
                 <div className='profile'>
                     <Link to='/profile'><img src={logo} alt='logo'/></Link>
