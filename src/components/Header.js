d84eadf53327676d018c28abaf4a2337e0da9d89
Cwaysi <cwaysimorison@outlook.com>
2024-07-07 18:19:01 +0000

    icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import logo from '../user.png';
 import {Link} from "react-router-dom"
-
-
+import HomeIcon from '@mui/icons-material/Home';
+import TaskIcon from '@mui/icons-material/Task';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import Diversity2Icon from '@mui/icons-material/Diversity2';
+import WorkIcon from '@mui/icons-material/Work';
 
 function Header(){
     return (
         <nav className='App-header'>
-            <h1 className='headername'>ISD-Team</h1>
+            <h1 className='headername'>ISD-Team  <Diversity2Icon color='warning'/></h1>
             <div className='nav--right'>
                 <h4>Project Tracker</h4>
-                <Link to="/" style={{ textDecoration: 'none'}}><p className='nav--button'>Home</p></Link>
-                <Link to="/projects" style={{ textDecoration: 'none'}} ><p className='nav--button'>Projects</p></Link>
-                <Link to="/stakeholder" style={{ textDecoration: 'none'}} ><p className='nav--button'>Stakeholders</p></Link>
-                <Link to="/tasks" style={{ textDecoration: 'none'}} ><p className='nav--button'>Tasks</p></Link>
+                <Link to="/" style={{ textDecoration: 'none'}}><p className='nav--button'><HomeIcon />  <span className='hide'>Home</span></p></Link>
+                <Link to="/projects" style={{ textDecoration: 'none'}} ><p className='nav--button'><WorkIcon />  <span className='hide'>Projects</span></p></Link>
+                <Link to="/stakeholder" style={{ textDecoration: 'none'}} ><p className='nav--button'><Diversity3Icon />  <span className='hide'>Stakeholders</span></p></Link>
+                <Link to="/tasks" style={{ textDecoration: 'none'}} ><p className='nav--button'><TaskIcon />  <span className='hide'> Tasks</span></p></Link>
               
-                <Link to="/register" style={{ textDecoration: 'none'}} ><p className='nav--button'>Register</p></Link>
+                <Link to="/register" style={{ textDecoration: 'none'}} ><p className='nav--button'><HowToRegIcon />  <span className='hide'>Register</span></p></Link>
                 <Link to="/logout" style={{ textDecoration: 'none'}} ><p className='nav--buttonLogout'>Logout</p></Link>
                
                 {/* <div className='profile'> */}
